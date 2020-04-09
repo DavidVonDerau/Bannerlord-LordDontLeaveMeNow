5483e2b904004bf6766de02d952b0fb50f364df4
David Von Derau <dvonderau@cobaltblue.us>
2020-04-08 19:07:03 -0700

    Test mod works in Bannerlord.
    Followed https://docs.bannerlordmodding.com/_tutorials/basic-csharp-mod.

---
+++ LordDontLeaveMeNow/SubModule.cs
@@ -0,0 +1,20 @@
+﻿using TaleWorlds.Core;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
+
+namespace LordDontLeaveMeNow
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+
+            Module.CurrentModule.AddInitialStateOption(new InitialStateOption("Message",
+                new TextObject("Message", null),
+                9990,
+                () => { InformationManager.DisplayMessage(new InformationMessage("Hello World!")); },
+                false));
+        }
+    }
+}
